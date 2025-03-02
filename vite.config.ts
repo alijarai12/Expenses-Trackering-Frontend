a5ab8e7da6c2881ba92415dfc863944f41c42364
alijarai12 <94462933+alijarai12@users.noreply.github.com>
2025-03-02 15:49:41 +0545

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  
+   server: {
+    host: '0.0.0.0', // Allows access from outside the container
+    port: 5173,      // Ensure it runs on the expected port
+  },
 })
